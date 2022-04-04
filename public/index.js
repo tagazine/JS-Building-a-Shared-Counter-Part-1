1118b714049cc456049b0284170892b44ce36acd
Your Namegit config --global user.name Your <you@example.com>
2022-04-04 18:47:10 -0400

    First Draft

--- public/index.js
+++ public/index.js
@@ -1,8 +1,10 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
-
+    let response = await fetch('http://108.202.56.182');
+    let responseJSON = await response.json();
+    console.log(responseJSON);
     let countValue = 0;
 
     function increment(){
